fb134eb1c8ba82ba4ba52c305a0d9df36f9cbdee
Andris Jancevskis <andris.jancevskis@gmail.com>
2024-11-02 02:33:34 +0000

    Update models with Meta info

--- blog/models.py
+++ blog/models.py
@@ -16,11 +16,23 @@ class Post(models.Model):
     updated_on = models.DateTimeField(auto_now=True)
     status = models.IntegerField(choices=STATUS, default=0, verbose_name='Post Status')
 
+    class Meta:
+        ordering = ['-created_on', 'author']
+
+    def __str__(self):
+        return f"The title of this post is: {self.title}"
+
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='Post')
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='Author')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='Commenter')
     body = models.TextField(verbose_name='Comment')
     created_on = models.DateTimeField(auto_now_add=True)
     approved = models.BooleanField(default=False, verbose_name='Approved')
-    
+
+    class Meta:
+        ordering = ['-created_on',]
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.author}"
+
